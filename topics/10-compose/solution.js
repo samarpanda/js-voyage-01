60b5d4e9f3e0895874d91f76ba6a468d8d8e4415
Samar Panda <er.samarpanda@gmail.com>
2016-04-13 15:56:42 +0530

    compose

---
+++ topics/10-compose/solution.js
@@ -0,0 +1,25 @@
+/**
+ * Compose function
+ */
+function compose(g, f){
+  return function(x){
+    return g(f(x));
+  }
+}
+
+/**
+ * Reverse String
+ */
+function reverse(s){
+  return s.split('').reverse().join('');
+}
+
+/**
+ * Capitalize
+ */
+function properNoun(s){
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+var revCap = compose(reverse, properNoun);
+console.log(revCap("samar"));
